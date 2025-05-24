b8c909044ce85b044174fdc00bec7c095ae0505a
NoahKyoot <correlatecynically@gmail.com>
2025-05-24 10:46:56 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 
 function CompetitionDetail() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-white">
+    <div className="min-h-screen bg-white p-10">
       <h1 className="text-3xl font-bold text-pink-600">Competition Details Coming Soon</h1>
       <p className="mt-4 text-gray-700">This will show detailed info based on the event selected.</p>
     </div>
@@ -14,11 +13,11 @@ function CompetitionDetail() {
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/competitions/:id" element={<CompetitionDetail />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
