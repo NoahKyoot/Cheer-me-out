0a4d5b00a2fed64772c0f4d0b29db8987c170447
NoahKyoot <correlatecynically@gmail.com>
2025-05-28 20:56:54 -0500

    Update HomePage.jsx

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -181,7 +181,7 @@ export default function HomePage() {
           <div className="text-center">
             <h4 className="text-md sm:text-lg md:text-xl font-semibold text-slate-100 whitespace-nowrap">
               {/* THIS IS THE CORRECTED LINE: */}
-              {format(weekStart, 'MMMM d')} – {format(weekEnd, 'MMMM d,<y_bin_46>)}
+          {format(weekStart, 'MMMM d')} – {format(weekEnd, 'MMMM d, yyyy')}
             </h4>
             {weekOffset !== 0 && (
               <button
